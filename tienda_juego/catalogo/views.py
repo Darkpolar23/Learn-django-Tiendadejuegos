8252bfee46f0f04ddc1b7baecd37583253cd7f94
jose colon <josecolonpena740@gmail.com>
2026-02-28 00:18:19 -0400

    Refactor code structure for improved readability and maintainability

--- tienda_juego/catalogo/views.py
+++ tienda_juego/catalogo/views.py
@@ -1,20 +1,9 @@
 from django.shortcuts import render
+from catalogo.models import Juego
 
 # Create your views here.
 def listado_juegos(request):
-    juegos = [{"nombre": "dogo racing", "precio":29.99, "plataformas": 
-               ["PC", "PS4","PS5","Xbox One"]},
-              {"nombre": "Naruto Strom 4", "precio":39.99, "plataformas":
-               ["PC", "PS4","PS5","Xbox One"]},
-              {"nombre": "Final Fantasy 15", "precio":49.99, "plataformas":
-               ["PC", "PS4","PS5","Xbox One"]}, 
-              {"nombre": "The Witcher 3", "precio":59.99, "plataformas":
-               ["PC", "PS4","PS5","Xbox One"]},
-                {"nombre": "GTA V", "precio":19.99, "plataformas":
-                 ["PC", "PS4","PS5","Xbox One"]},
-                {"nombre": "Red Dead Redemption 2", "precio":59.99, "plataformas":
-                 ["PC", "PS4","PS5","Xbox One"]},
-               ]
+    juegos = Juego.objects.all()
     
     contexto_catalogo_juegos = {"listado_juegos": juegos}
     return render(request, "catalogo/listado_juegos.html",
