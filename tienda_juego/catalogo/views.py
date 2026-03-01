da7aa53df351ac87dc9d6b4528e5ec39d54c51b7
jose colon <josecolonpena740@gmail.com>
2026-03-01 01:19:49 -0400

    admin creado y mejora del listado de juegos

--- tienda_juego/catalogo/views.py
+++ tienda_juego/catalogo/views.py
@@ -3,7 +3,7 @@ from catalogo.models import Juego
 
 # Create your views here.
 def listado_juegos(request):
-    juegos = Juego.objects.all()
+    juegos = Juego.objects.all() #trae todos los juegos de la base de datos
     
     contexto_catalogo_juegos = {"listado_juegos": juegos}
     return render(request, "catalogo/listado_juegos.html",
